f901b326d116ac7832acaa67b711a77563db9d55
mutlr <rmutlu01@hotmail.com>
2023-03-28 20:52:21 +0300

    Bim
    Bam

--- code/js.js
+++ code/js.js
@@ -1,14 +1,21 @@
+document.getElementById("add-Note").addEventListener("click", function() {
+    makeCard("name", "text", "important");
+});
 
-document.getElementById("add-Note").addEventListener("click", () => {
-    let name = document.getElementById("name");
-    let teksti = document.getElementById("text");
-    let important = document.getElementById("important").checked;
+document.getElementById("FSadd-note").addEventListener("click", function() {
+    makeCard("FSname", "FStext", "FSimportant");
+});
+function makeCard(nameID, textID, checkbox) {
+    console.log("Hello");
+    let name = document.getElementById(nameID);
+    let teksti = document.getElementById(textID);
+    let important = document.getElementById(checkbox).checked;
     
-
+    
     let date = new Date();
     let boxText = `(${name.value}) \t ${date.getUTCDate()}.${date.getUTCMonth()}.${date.getUTCFullYear()} \t ${date.getHours()}:${date.getMinutes()}`;
     let boxHeader = createElements("div", "note-header", boxText);
-
+    
     let text = document.createElement("p");
     text.className = "note-text";
     text.textContent = teksti.value;
@@ -25,13 +32,14 @@ document.getElementById("add-Note").addEventListener("click", () => {
     box.append(btnBox);
     
     if (important) {
-        box.style.borderLeft = "12px solid orange";
+        box.style.borderColor = "orange";
     }
-
+    
     document.querySelector(".main").append(box);
     name.value = "";
     teksti.value = "";
-})
+    console.log("hi");
+}
 
 function deleteCard(e) {
     document.querySelector(".main").removeChild(e.target.parentNode.parentNode);
