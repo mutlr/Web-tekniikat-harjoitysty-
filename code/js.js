b540fa157f23a8c5cfca979d84878f3192008665
mutlr <rmutlu01@hotmail.com>
2023-03-28 11:02:09 +0300

    Added JS and styling

--- code/js.js
+++ code/js.js
@@ -1,33 +1,45 @@
-document.getElementById("add").addEventListener("click", () => {
-    let name = document.getElementById("name").value;
-    let teksti = document.getElementById("text").value;
-    let check = document.getElementById("important").checked;
+
+document.getElementById("add-Note").addEventListener("click", () => {
+    let name = document.getElementById("name");
+    let teksti = document.getElementById("text");
+    let important = document.getElementById("important").checked;
     
-    let boxHeader = document.createElement("div");
+
     let date = new Date();
-    let space = "&ensp;"
-    boxHeader.className = "box-header";
-    boxHeader.textContent = `(${name}) \t ${date.getUTCDate()}.${date.getUTCMonth()}.${date.getUTCFullYear()} \t ${date.getHours()}.${date.getMinutes()}`;
+    let boxText = `(${name.value}) \t ${date.getUTCDate()}.${date.getUTCMonth()}.${date.getUTCFullYear()} \t ${date.getHours()}:${date.getMinutes()}`;
+    let boxHeader = createElements("div", "note-header", boxText);
 
     let text = document.createElement("p");
     text.className = "note-text";
-    text.textContent = teksti;
+    text.textContent = teksti.value;
     
-    
-    let button = document.createElement("button");
-    button.className = "delete btn btn-danger";
+    let btnBox = createElements("div", "btn-box");
+    let button = createElements("button", "btn btn-danger", "Delete");
     button.type = "button";
-    button.textContent = "Delete";
+    btnBox.append(button);
+    btnBox.addEventListener("click", deleteCard);
     
-    let box = document.createElement("div");
-    box.className = "box";
+    let box = createElements("div", "note-box", "");
     box.append(boxHeader);
     box.append(text);
-    box.append(button);
-
-    document.querySelector(".card-container").append(box);
-
-    if (check) {
-        box.style.borderColor = "blue";
+    box.append(btnBox);
+    
+    if (important) {
+        box.style.borderLeft = "12px solid orange";
     }
+
+    document.querySelector(".main").append(box);
+    name.value = "";
+    teksti.value = "";
 })
+
+function deleteCard(e) {
+    document.querySelector(".main").removeChild(e.target.parentNode.parentNode);
+}
+
+function createElements(type, classname, content) {
+    let element = document.createElement(type);
+    element.className = classname;
+    element.textContent = content;
+    return element;
+}
