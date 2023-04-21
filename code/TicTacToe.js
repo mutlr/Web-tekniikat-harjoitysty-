c275bebab060124992f616a4a24fcf5739bce0d2
mutlr <rmutlu01@hotmail.com>
2023-04-21 13:04:13 +0300

    TicTacToe

---
+++ code/TicTacToe.js
@@ -0,0 +1,130 @@
+
+const Player = (marker) => {
+    let score = 0;
+    const returnMarker = () => marker;
+    const increaseScore = () => score++;
+    const returnScore = () => {
+        increaseScore();
+        return score;
+    }
+    return {returnMarker, returnScore};
+}
+
+const playerX = Player("X");
+const playerO = Player("O");
+
+let count = 0;
+let currentPlayer;
+const gameboard = ["", "", "", "", "", "", "", "", ""];
+init();
+
+function init() {
+    makeBoard();
+    addListener();
+    count = 0;
+
+}
+function makeBoard() {
+    let index = 0
+    const gameContainer = document.querySelector(".game-container");
+    for (let i = 0; i < 3; i++) {
+        const row = makeElement("div", "row");
+        for (let j = 0; j < 3; j++) {
+            let el = makeElement("div", "box", "");
+            el.setAttribute("data-index", index++);
+            row.append(el);
+        }
+        gameContainer.append(row);
+    }
+}
+function restart() {
+    gameboard.fill("", 0, 9);
+    const rows = document.querySelectorAll(".row");
+    rows.forEach((row) => {
+        document.querySelector(".game-container").removeChild(row);
+    })
+}
+function makeElement (type, classname, text) {
+    const el = document.createElement(type);
+    el.className = classname;
+    el.textContent = text;
+    return el;
+}
+function start() {
+    currentPlayer = playerX;
+    restart();
+    init();
+}
+function winnerScreen(value, player) {
+    document.querySelector(".winscreen").classList.remove("display");
+    console.log(player);
+    if (value === 1) {
+        document.querySelector(".tie").classList.remove("display");
+    } else {
+        document.querySelector(".win").classList.remove("display");
+        document.getElementById("winnerText").textContent = player.returnMarker();
+        document.getElementById(player.returnMarker()).textContent = player.returnScore();
+    }
+    document.getElementById("newGame").addEventListener("click", () => {
+        document.querySelector(".winscreen").classList.add("display");
+        document.querySelector(".win").classList.add("display");
+        document.querySelector(".tie").classList.add("display");
+        start();
+    });
+}
+
+
+function winner() {
+    if (gameboard[0] === currentPlayer.returnMarker() && gameboard[1] === currentPlayer.returnMarker() && gameboard[2] === currentPlayer.returnMarker()) {
+        return true;
+    } else if (gameboard[3] === currentPlayer.returnMarker() && gameboard[4] === currentPlayer.returnMarker() && gameboard[5] === currentPlayer.returnMarker()) {
+        return true;
+    } else if (gameboard[6] === currentPlayer.returnMarker() && gameboard[7] === currentPlayer.returnMarker() && gameboard[8] === currentPlayer.returnMarker()) {
+        return true;
+    } else if (gameboard[0] === currentPlayer.returnMarker() && gameboard[4] === currentPlayer.returnMarker() && gameboard[8] === currentPlayer.returnMarker()) {
+        return true;
+    } else if (gameboard[6] === currentPlayer.returnMarker() && gameboard[2] === currentPlayer.returnMarker() && gameboard[4] === currentPlayer.returnMarker()) {
+        return true;
+    } else if (gameboard[0] === currentPlayer.returnMarker() && gameboard[3] === currentPlayer.returnMarker() && gameboard[6] === currentPlayer.returnMarker()) {
+        return true;
+    } else if (gameboard[1] === currentPlayer.returnMarker() && gameboard[4] === currentPlayer.returnMarker() && gameboard[7] === currentPlayer.returnMarker()) {
+        return true;
+    } else if (gameboard[2] === currentPlayer.returnMarker() && gameboard[5] === currentPlayer.returnMarker() && gameboard[8] === currentPlayer.returnMarker()) {
+        return true;
+    }
+    if (!gameboard.includes("")) {
+        console.log("Hei");
+        return 1;
+    }
+    return false;
+}
+function addListener() {
+    const boxes = document.querySelectorAll(".box");
+    boxes.forEach((btn) => {
+        btn.addEventListener("click", (e) => {
+            addToBox(e);
+            let winnerValue = winner(currentPlayer);
+            if (winnerValue === true || winnerValue === 1) {
+                winnerScreen(winnerValue, currentPlayer);
+            } else {
+                switchPlayers();
+            }
+        }, {once: true});
+    });
+}
+
+function addToBox(e) {
+        e.target.textContent = currentPlayer.returnMarker();
+    gameboard[e.target.dataset.index] = currentPlayer.returnMarker();
+    console.log(currentPlayer.returnMarker());
+}
+function switchPlayers() {
+    if (currentPlayer === playerX) {
+        currentPlayer = playerO;
+        return
+    }
+    currentPlayer = playerX;
+}
+
+window.onload = start();
+document.getElementById("restartBtn").addEventListener("click", start);
